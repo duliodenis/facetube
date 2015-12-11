ce82a503662411b57bcc87d19d01f4898b6fc636
Dulio Denis <dulio.denis@yahoo.com>
2015-12-11 12:25:07 -0500

    Added Firebase SDK via Cocoapods

--- FaceTube/FaceTube/ViewController.swift
+++ FaceTube/FaceTube/ViewController.swift
@@ -7,11 +7,26 @@
 //
 
 import UIKit
+import FBSDKCoreKit
+import FBSDKLoginKit
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+    }
+    
+    @IBAction func fbButtonTapped(sender: UIButton!) {
+        let facebookLogin = FBSDKLoginManager()
+        facebookLogin.logInWithReadPermissions(["email"]) { (facebookResult:FBSDKLoginManagerLoginResult!, facebookError: NSError!) -> Void in
+            if facebookError != nil {
+                print("Facebook Login Failed. Error \(facebookError.debugDescription)")
+            } else {
+                let accessToken = FBSDKAccessToken.currentAccessToken().tokenString
+                print("Successfully logged in with Facebook: \(accessToken)")
+            }
+        }
+        
     }
 
 }
